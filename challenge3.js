c151ed30756d5b97c104bb3cf1efd89399b79b73
Digital Fabric / David <dwawryko@gmail.com>
2019-11-03 19:44:31 -0500

    Added challenge 3

---
+++ challenge3.js
@@ -0,0 +1,12 @@
+function countVotes(name, votes){
+
+if (name === 'Jim'){
+ votes[0] = votes[0] + 1
+} else if (name === 'Sally'){
+ votes[1] = votes[1] + 1
+} else if (name === 'Beth'){
+ votes[2] = votes[2] + 1
+} 
+return votes
+
+}
