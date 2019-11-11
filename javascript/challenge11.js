f46f50b5df08cd29d213b08baee22c3984744bb8
Digital Fabric / David <dwawryko@gmail.com>
2019-11-11 12:09:18 -0500

    Fixed formatting for challenge 11

--- javascript/challenge11.js
+++ javascript/challenge11.js
@@ -1,7 +1,7 @@
-const busTimes = (buses) => {
+const busTimes = buses => {
   const times = {}
   for (let [key, value] of Object.entries(buses)) {
-    times[key] = value[distance]/value[speed]
+    times[key] = value.distance/value.speed
     }
     console.log(times)
     return times
