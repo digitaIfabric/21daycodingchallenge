1c6ecf0073ba7941c4c1d32c5968d5bc6cdb3b2c
Digital Fabric / David <dwawryko@gmail.com>
2019-11-15 11:44:39 -0500

    Added challenge 15 solution

---
+++ javascript/challenge15.js
@@ -0,0 +1,16 @@
+const finalPosition = (moves) => {
+  let xTotal = 0
+  let yTotal = 0
+  for (let i = 0; i <= moves.length; i++){
+    if (moves[i] === 'north'){
+      yTotal++
+    } else if (moves[i] === 'south'){
+      yTotal--
+    } else if (moves[i] === 'west'){
+      xTotal--
+    } else if (moves[i] === 'east'){
+      xTotal++
+    }
+  }
+  return [xTotal, yTotal]
+}
