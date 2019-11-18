253c8d47b7cef10e37f5a05ec1f84f13bf788a15
Digital Fabric / David <dwawryko@gmail.com>
2019-11-18 11:32:14 -0500

    Refactored bestOdds function

--- javascript/challenge18.js
+++ javascript/challenge18.js
@@ -17,17 +17,6 @@ const bestOdds = (tickets, raffleEntries) => {
     let rT = Object.keys(tally).map(i => tally[i])
     let rE = Object.keys(raffleEntries).map(i => raffleEntries[i])
     let result = rT.map(function (n, i) { return n / rE[i]; });
-    console.log(result)
-    if (result[0] >= result[1]) {
-        if (result[2] > result[0]) {
-            return "You have the best odds of winning the blue raffle."
-        }
-        return "You have the best odds of winning the red raffle."
-    } else {
-        if (result[2] >= result[1]) { 
-            return "You have the best odds of winning the blue raffle."
-        } else {
-          return "You have the best odds of winning the green raffle."
-        }
-    }
+    let index = result.indexOf(Math.max(...result));         
+    return `You have the best odds of winning the ${Object.keys(raffleEntries)[index]} raffle.`
 };
