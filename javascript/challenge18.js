96ba41e1b2a458c4746f73eaa306e4063ca1a62d
Digital Fabric / David <dwawryko@gmail.com>
2019-11-18 10:34:55 -0500

    Challenge 18 to be refactored

---
+++ javascript/challenge18.js
@@ -0,0 +1,35 @@
+const countTickets = (tickets) => {
+    let object = { red: 0, green: 0, blue: 0 };
+    for (let i = 0; i < tickets.length; i++) {
+        if (tickets[i] === 'red') {
+            object['red'] = object['red'] + 1;
+        }
+        else if (tickets[i] === 'green') {
+            object['green'] = object['green'] + 1;
+        }
+        else if (tickets[i] === 'blue') {
+            object['blue'] = object['blue'] + 1;
+        }
+    }
+    return object;
+};
+
+const bestOdds = (tickets, raffleEntries) => {
+    let tally = countTickets(tickets)
+    let rT = Object.keys(tally).map(i => tally[i])
+    let rE = Object.keys(raffleEntries).map(i => raffleEntries[i])
+    let result = rT.map(function (n, i) { return n / rE[i]; });
+    console.log(result)
+    if (result[0] >= result[1]) {
+        if (result[2] > result[0]) {
+            return "You have the best odds of winning the blue raffle."
+        }
+        return "You have the best odds of winning the red raffle."
+    } else {
+        if (result[2] >= result[1]) { 
+            return "You have the best odds of winning the blue raffle."
+        } else {
+          return "You have the best odds of winning the green raffle."
+        }
+    }
+};
