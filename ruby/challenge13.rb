b61f578f45d9019ca501b727492ec9b8938de372
Digital Fabric / David <dwawryko@gmail.com>
2019-12-01 12:40:06 -0500

    Refactored challenge 13 ruby

--- ruby/challenge13.rb
+++ ruby/challenge13.rb
@@ -1,19 +1,19 @@
-def lightsOn(lights):
-  for light in lights:
+def lightsOn(lights)
+  for light in lights
     light.on = True
   end
   return lights
 end
 
-def lightsOff(lights):
-    for light in lights:
+def lightsOff(lights)
+    for light in lights
       light.on = False
     end
     return lights;
-
-def toggleLights(lights, lightsAreOn):
-  for light in lights:
-    light.on = ~light.on
+  
+def toggleLights(lights, lightsAreOn)
+  for light in lights
+    light.on = !light.on
   end
   return lights
 end
