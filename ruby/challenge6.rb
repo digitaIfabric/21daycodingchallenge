e792493586635a90234bc4cd5cff975d47a23741
Digital Fabric / David <dwawryko@gmail.com>
2019-12-01 13:00:51 -0500

    Added challenge 6 rb

---
+++ ruby/challenge6.rb
@@ -0,0 +1,26 @@
+def voterTurnout(voter_signatures, voter_ids)
+    if (voter_signatures.length != voter_ids.length)
+        return false
+    end
+
+    truth = []
+    for ii in 0..voter_ids.length do
+        if voter_signatures[ii] == voter_ids[ii]
+            truth[ii] = true
+        else 
+            truth[ii] = false
+        end
+    end    
+    
+    if truth.any?
+        puts 'All clear, we can count the votes!'
+        return 'All clear, we can count the votes!'
+    else
+        return 'FRAUD!'
+    end
+    
+end
+
+# voter_signatures = ['Bill Billiamson','Kate Etak','Brandon Brandonus','Simon Simonson','Jane Janesford']
+# voter_ids = ['Bill Billiamson','Kate Etak','Brandon Brandonus','Simon Simonson','Jane Janesford']       
+# voterTurnout(voter_signatures, voter_ids)
